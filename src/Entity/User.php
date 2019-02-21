060ce78439140d10ebd1a06a2bdeac59f039375f
backaligth <antoine.soros@orange.fr>
2019-02-21 10:39:18 +0100

    creation of : -User entity -UserLoginType Form -USerSignUpType form

---
+++ src/Entity/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userEmail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userPassword;
+    
+    private $userConfirmPassword;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(string $userName): self
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    public function getUserEmail(): ?string
+    {
+        return $this->userEmail;
+    }
+
+    public function setUserEmail(string $userEmail): self
+    {
+        $this->userEmail = $userEmail;
+
+        return $this;
+    }
+
+    public function getUserPassword(): ?string
+    {
+        return $this->userPassword;
+    }
+
+    public function setUserPassword(string $userPassword): self
+    {
+        $this->userPassword = $userPassword;
+
+        return $this;
+    }
+    function getUserConfirmPassword() {
+        return $this->userConfirmPassword;
+    }
+
+    function setUserConfirmPassword($userConfirmPassword) {
+        $this->userConfirmPassword = $userConfirmPassword;
+        return $this;
+    }
+
+
+}
